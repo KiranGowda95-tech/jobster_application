e654c23945e845d8824f8466afb791326cc8bfc2
Kiran <kirankumardn74@gmail.com>
2023-11-16 06:45:12 +0530

    register file updated

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const FormRow = () => {
-  return <div>FormRow</div>;
+const FormRow = ({ type, name, value, handleChange, labelText }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {name}
+      </label>
+      <input
+        type="text"
+        name="name"
+        value={value.name}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </div>
+  );
 };
 
 export default FormRow;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Logo } from "../components";
+import { Logo, FormRow } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 
 const initialState = {
@@ -13,34 +13,62 @@ function Register() {
   const [values, setValues] = useState(initialState);
 
   const handleChange = (e) => {
-    console.log(e.target);
+    //console.log(e.target);
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}:${value}`);
+    setValues({ ...values, [name]: value });
   };
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      console.log("Please fill out all the fields");
+    }
+    //console.log(e.target);
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
   };
 
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{values.isMember ? "login" : "register"}</h3>
         {/* name field */}
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
+        {!values.isMember && (
+          <FormRow
             type="text"
             name="name"
             value={values.name}
-            onChange={handleChange}
-            className="form-input"
+            handleChange={handleChange}
           />
-        </div>
+        )}
+        {/* email field */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
         <button type="submit" className="btn btn-block">
           submit
         </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
